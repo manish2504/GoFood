61f0aa87f076abeda50f77f3b680556c4fbba0d6
Manish Kumar Yadav <manishky254@gmail.com>
2025-02-17 23:57:11 +0530

    updated cart 2

--- src/screens/Cart.jsx
+++ src/screens/Cart.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Delete from '@material-ui/icons/Delete'
+//import Delete from '@material-ui/icons/Delete'
 import { useCart, useDispatchCart } from '../components/contextReducer'
 
 export default function Cart() {
