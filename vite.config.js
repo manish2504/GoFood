d38154469c13ddbe14a19ee0d532aeb432327175
Manish Kumar Yadav <manishky254@gmail.com>
2025-02-17 23:38:48 +0530

    proxy updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      "/api":"http://localhost:5000",
+      "/api":"https://gofood-backend-dc6g.onrender.com",
     },
   },
   plugins: [react()],
